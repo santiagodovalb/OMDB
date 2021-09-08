17a8fc5e0c5d51e62be4bae50ed0c2e3cbcfad34
santikundera <santikundera@gmail.com>
2021-09-08 14:15:10 -0300

    sequelize error3

--- api/models/db.js
+++ api/models/db.js
@@ -1,16 +1,17 @@
-const Sequelize = require('sequelize')
+const Sequelize = require("sequelize");
 
 sequelize = new Sequelize(process.env.DATABASE_URL, {
-    database: "d7tpsqnm7urobp",
+  database: "d7tpsqnm7urobp",
   username: "cugdkkfsokhvqe",
   password: "3a796aa16e18f9840c46683aeadd7402f3fabf18a98365f143b45c8a86e1df29",
   host: "ec2-52-7-159-155.compute-1.amazonaws.com",
   port: 5432,
-    dialect: 'postgres',
-    dialectOptions: {
-        ssl: true,
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
         rejectUnauthorized: false
     }
+  },
 });
 
-module.exports = sequelize
+module.exports = sequelize;
