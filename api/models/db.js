9283438ccc397499d74acfb74362e7be349d6881
santikundera <santikundera@gmail.com>
2021-09-08 13:24:19 -0300

    heroku deploy4

--- api/models/db.js
+++ api/models/db.js
@@ -1,23 +1,10 @@
-const { Client } = require('pg');
-
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+const Sequelize = require('sequelize')
+sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+        ssl: true
+    }
 });
 
-client.connect();
-
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  client.end();
-});
-
-// const Sequelize = require('sequelize')
-// const sequelize = new Sequelize('postgres://postgres@localhost/omdb', {logging: false, dialect: 'postgres'})
-
-module.exports = client
+module.exports = sequelize
