831d8d668678de14a9314a60b286baba5e3c1d6d
santikundera <santikundera@gmail.com>
2021-09-10 10:31:39 -0300

    about

--- src/containers/About.jsx
+++ src/containers/About.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 function About() {
     return (
         <div className='about'>
-            <p>This site was developed simply as practice. It's based in the <a href='https://www.omdbapi.com/'>OMDB API</a>, a large, open-access movie database. It was made by Santiago Doval in the context of the Plataforma 5 Full-Stack coding bootcamp.</p>
+            <p>This site was developed simply as practice. It's based in the <a href='https://www.omdbapi.com/'>OMDB API</a>, a large, open-access movie database. It was made by Santiago Doval, powered by Node JS and React.</p>
             <p>---</p>
-            <p>Este sitio fue desarrollado simplemente a modo de practica. Esta basado en la <a href='https://www.omdbapi.com/'>OMDB API</a>, una gran base de datos de peliculas de libre acceso. Fue realizado por Santiago Doval en el contexto del bootcamp Full-Stack de Plataforma 5.</p>
+            <p>Este sitio fue desarrollado simplemente a modo de practica. Esta basado en la <a href='https://www.omdbapi.com/'>OMDB API</a>, una gran base de datos de peliculas de libre acceso. Fue realizado por Santiago Doval, utilizando Node JS y React.</p>
         </div>
     )
 }
